3136c6184877e794b133972330aab76981166aad
Bastian Schuchardt <bschuchardt@condor.e4.physik.tu-dortmund.de>
2025-04-08 13:42:35 +0200

    Added basic plot.py

---
+++ plot.py
@@ -0,0 +1,28 @@
+"""Plots datasets."""
+
+# ---------- Imports ---------- #
+import os
+import argparse
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from config import (
+    columns,
+    output_path
+)
+
+from io_utils import ensure_dir_exists
+
+# ---------- Argument Parser ---------- #
+
+
+# ---------- Helper Functions ---------- #
+
+
+# ---------- Main Function ---------- #
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
