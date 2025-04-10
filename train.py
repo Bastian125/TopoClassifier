631559cf380c0cf426977a46d691e578dce97b9d
Bastian Schuchardt <bschuchardt@veritate.e4.physik.tu-dortmund.de>
2025-04-10 13:43:17 +0200

    Added non-functional train.py

---
+++ train.py
@@ -0,0 +1,24 @@
+"""
+Train different ML models on given input features.
+"""
+
+# ---------- Imports ---------- #
+import os
+import argparse
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+
+# ---------- Helper Functions ---------- #
+
+# ---------- Main Function ---------- #
+def main():
+    print("Here is a main function!")
+
+if __name__ == "__main__":
+    main()
