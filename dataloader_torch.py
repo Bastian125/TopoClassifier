c182567f810bea4e8a828199f1777357e36d8353
Bastian Schuchardt <bschuchardt@veritate.e4.physik.tu-dortmund.de>
2025-05-07 15:21:47 +0200

    Added pytorch alternative. Still needs tweaking for faster training

---
+++ dataloader_torch.py
@@ -0,0 +1,31 @@
+import h5py
+import numpy as np
+
+import torch
+import tqdm
+from torch.utils.data import Dataset
+
+
+class HDF5Dataset(Dataset):
+    """
+    PyTorch dataset for loading normalized ATLAS features from an HDF5 file.
+    Each sample includes a set of features and a binary label.
+    """
+
+    def __init__(self, file_path, feature_keys):
+        self.file_path = file_path
+        self.feature_keys = feature_keys
+
+        # Load entire dataset into memory
+        with h5py.File(self.file_path, "r") as f:
+            self.data = torch.tensor(
+                np.stack([f[key][:] for key in self.feature_keys], axis=1),
+                dtype=torch.float32,
+            )
+            self.labels = torch.tensor(f["label"][:], dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
