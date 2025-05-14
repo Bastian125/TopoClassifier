d0dcb6125c3295bf5fa0f8429cb083c1ef99ce59
Bastian Schuchardt <bschuchardt@gpu02.e4.physik.tu-dortmund.de>
2025-05-14 11:35:09 +0200

    Added dataloader that trains on response

--- dataloader.py
+++ dataloader.py
@@ -29,3 +29,22 @@ class HDF5Dataset(Dataset):
 
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
+
+class HDF5DatasetResponse(Dataset):
+    def __init__(self, file_path, feature_keys):
+        self.file_path = file_path
+        self.feature_keys = feature_keys
+
+        # Load entire dataset into memory
+        with h5py.File(self.file_path, "r") as f:
+            self.data = torch.tensor(
+                np.stack([f[key][:] for key in self.feature_keys], axis=1),
+                dtype=torch.float32,
+            )
+            self.labels = torch.tensor(f["cluster_response"][:], dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
