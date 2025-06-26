27915443f208ba6e9c98488ea78811c2a7a0a647
Bastian Schuchardt <bschuchardt@gpu02.e4.physik.tu-dortmund.de>
2025-06-26 12:13:19 +0200

    Corrected GAT

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ All models used in train.py are defined here.
 # ---------- Imports ---------- #
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch_geometric.nn import GATConv
 
 
@@ -54,51 +55,47 @@ class GATNet(nn.Module):
     def __init__(self, in_channels):
         super(GATNet, self).__init__()
 
+        # GAT layers
         self.conv1 = GATConv(in_channels, 16, heads=8, dropout=0.1)
         self.conv2 = GATConv(16 * 8, 32, heads=8, dropout=0.1)
 
-        self.Linear_node1 = torch.nn.Linear(16, 32)
-        self.Linear_node2 = torch.nn.Linear(32, 32)
+        # Feed-forward pathway for raw input features
+        self.Linear_node1 = nn.Linear(in_channels, 32)
+        self.Linear_node2 = nn.Linear(32, 32)
 
-        self.Linear_node_After1 = torch.nn.Linear(128, 64)
-        self.Linear_node_After2 = torch.nn.Linear(64, 64)
+        # MLP on GAT conv1 output
+        self.Linear_node_After1 = nn.Linear(128, 64)
+        self.Linear_node_After2 = nn.Linear(64, 64)
 
-        self.Linear_node_After3 = torch.nn.Linear(256, 128)
-        self.Linear_node_After4 = torch.nn.Linear(128, 64)
+        # MLP on GAT conv2 output
+        self.Linear_node_After3 = nn.Linear(256, 128)
+        self.Linear_node_After4 = nn.Linear(128, 64)
 
-        self.Linear_final1 = torch.nn.Linear(160, 128)
-        self.Linear_final2 = torch.nn.Linear(128, 64)
-        self.Linear_final3 = torch.nn.Linear(64, 1)
+        # Final classifier head
+        self.Linear_final1 = nn.Linear(160, 128)
+        self.Linear_final2 = nn.Linear(128, 64)
+        self.Linear_final3 = nn.Linear(64, 1)
 
     def forward(self, x, edge_index):
+        # Process raw input features
+        gg1 = F.relu(self.Linear_node1(x))
+        gg2 = F.relu(self.Linear_node2(gg1))
 
-        gg1 = self.Linear_node1(x)
-        gg1 = torch.relu(gg1)
+        # GAT path
+        x1 = F.relu(self.conv1(x, edge_index))  # [N, 128]
+        x2 = F.relu(self.conv2(x1, edge_index))  # [N, 256]
 
-        gg2 = self.Linear_node2(gg1)
-        gg2 = torch.relu(gg2)
+        # Feed-forward on x1 and x2
+        x_after1 = F.relu(self.Linear_node_After1(x1))
+        x_after1 = F.relu(self.Linear_node_After2(x_after1))
 
-        x1 = self.conv1(x, edge_index)
-        x1 = torch.relu(x1)
-        x2 = self.conv2(x1, edge_index)
-        x2 = torch.relu(x2)
+        x_after2 = F.relu(self.Linear_node_After3(x2))
+        x_after2 = F.relu(self.Linear_node_After4(x_after2))
 
-        x_after1 = self.Linear_node_After1(x1)
-        x_after1 = torch.relu(x_after1)
-        x_after1 = self.Linear_node_After2(x_after1)
-        x_after1 = torch.relu(x_after1)
-
-        x_after2 = self.Linear_node_After3(x2)
-        x_after2 = torch.relu(x_after2)
-        x_after2 = self.Linear_node_After4(x_after2)
-
-        x_after2 = torch.relu(x_after2)
-
-        xfinal = torch.cat((gg2, x_after1, x_after2), dim=1)
-        xfinal = self.Linear_final1(xfinal)
-        xfinal = torch.relu(xfinal)
-        xfinal = self.Linear_final2(xfinal)
-        xfinal = torch.relu(xfinal)
+        # Concatenate all processed representations
+        xfinal = torch.cat([gg2, x_after1, x_after2], dim=1)  # [N, 160]
+        xfinal = F.relu(self.Linear_final1(xfinal))
+        xfinal = F.relu(self.Linear_final2(xfinal))
         xfinal = self.Linear_final3(xfinal)
 
         return xfinal
