99346b1cf99d525512234ee67f1de6e859d68ff7
Bastian Schuchardt <bschuchardt@gpu02.e4.physik.tu-dortmund.de>
2025-08-08 13:37:26 +0200

    Formatting

--- train.py
+++ train.py
@@ -130,21 +130,18 @@ elif args.GAT:
 
 
 # Set learning rate and batch size depending on Run2 or Run3
-if "Run2" in train_dataset_str and "DNN" or "JetDNN" in model_str:
+if "Run2" in train_dataset_str and "DNN" or "JetDNN" or "GCN" in model_str:
     LEARNING_RATE = LEARNING_RATE_RUN2
     BATCH_SIZE = BATCH_SIZE_RUN2
 elif "Run2" in train_dataset_str and "GAT" in model_str:
     LEARNING_RATE = LEARNING_RATE_GNN
     BATCH_SIZE = BATCH_SIZE_RUN2
-elif "Run3" in train_dataset_str and "DNN" or "JetDNN" in model_str:
+elif "Run3" in train_dataset_str and "DNN" or "JetDNN" or "GCN" in model_str:
     LEARNING_RATE = LEARNING_RATE_RUN3
     BATCH_SIZE = BATCH_SIZE_RUN3
 elif "Run3" in train_dataset_str and "GAT" in model_str:
     LEARNING_RATE = LEARNING_RATE_GNN
     BATCH_SIZE = BATCH_SIZE_RUN3
-elif "Run2" in train_dataset_str and "GCN" in model_str:
-    
-
 
 # ---------- Helper Functions ---------- #
 def plot_training_history(history):
